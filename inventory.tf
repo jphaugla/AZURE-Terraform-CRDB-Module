55e59f9c4376ac51f2c376af2e1870b17ca2ab32
jphaugla <jasonhaugland@gmail.com>
2024-05-24 12:25:58 -0500

    add path module to inventory.tf

--- inventory.tf
+++ inventory.tf
@@ -1,6 +1,5 @@
 resource "local_file" "instances_file" {
-    filename = "${var.instances_inventory_directory}/${var.instances_inventory_file}"
-    content = templatefile("${var.inventory_template_file}",
+    content = templatefile("${path.module}/${var.inventory_template_file}",
         {
             crdb_public_names = "${join("\n", (azurerm_public_ip.crdb-ip.*.name) )}"
             crdb_public_ips = "${join("\n", (azurerm_public_ip.crdb-ip.*.ip_address) )}"
